68123a1bede60f595e2d2c713890fb0a3b83eeac
van son <vanson16032003@gmailcom>
2025-11-08 15:41:41 +0700

    update

--- src/db/migrations/20251107000001-create-settings.js
+++ src/db/migrations/20251107000001-create-settings.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.createTable("Settings", {
+        await queryInterface.createTable("settings", {
             id: {
                 allowNull: false,
                 autoIncrement: true,
@@ -39,6 +39,6 @@ module.exports = {
     },
 
     down: async (queryInterface, Sequelize) => {
-        await queryInterface.dropTable("Settings");
+        await queryInterface.dropTable("settings");
     },
 };
